6b6e209c3431b81f05900d6dcd97933bd82773bc
Griffin_Nader <naderebrahimi98@gmail.com>
2025-11-09 21:47:15 +0330

    Make a user function to register a new user

--- wyr.py
+++ wyr.py
@@ -1,12 +1,49 @@
+import random
+from datetime import date
+
 def main():
-    print(user())
+    file, gender = user()
 
 def user():
-    name = input("What's your name: ")
-    birth = input("Enter your birth date: ")
-    gen = input("If you're Male enter 'M' or not enter 'F': ")
+    today = str(date.today())
+    
+    try:
+        name = input("What's your name: ")
+        if len(name) < 3:
+            print("Invalid name input")
+            raise ValueError
+        
+        if not name[0:2].isalpha():
+            print("Your name can not begin with a number")
+            raise ValueError
 
-    return name, birth, gen
+        birth = input("Enter your birth date: ")
+        if not birth.isnumeric():
+            print("Your birth date must be a number")
+            raise ValueError
+        
+        if len(birth) != 4:
+            print("You must enter a 4-digit birth date")
+            raise ValueError
+
+        gen = input("If you're Male enter 'M' or not enter 'F': ").strip().upper()
+        id = random.randrange(1000, 2000)
+
+        # Append user's informations to users.csv
+        with open("Users/users.csv", 'a') as file:
+            file.writelines(f"{name}, {birth}, {gen}, {id}\n")
+
+        user_file = "Users/" + str(id).strip() + ".csv"
+
+        # Create a file to store user answers
+        with open(user_file, 'w') as file:
+            file.writelines(f'{today}\n')
+
+        return user_file, gen
+
+    except ValueError:
+        pass
+
 
 if __name__ == "__main__":
     main()
